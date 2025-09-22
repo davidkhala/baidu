4ee97eb0be27aac05a202f69ab5bf5bab6f06e35
David Liu <david-khala@hotmail.com>
2025-09-23 01:13:38 +0800

    add map

--- map/py/tests/test_geocode.py
+++ map/py/tests/test_geocode.py
@@ -12,6 +12,13 @@ class MyTestCase(unittest.TestCase):
         r = self.api.get_location("重慶市雲陽雙江中學校")
         print(r)
 
+    def test_location_en(self):
+
+        r = self.api.geocoding("Fuqing No. 1 High School")
+        self.assertEqual(r['level'],'购物') # mismatch
+        r = self.api.geocoding("福清一中")
+        self.assertEqual(r['level'],'教育') # match
+
 
 if __name__ == '__main__':
     unittest.main()
